2b404e7b73a62b5a08000d66840ef4d80d414763
Diablohu <diablohudream@gmail.com>
2017-01-09 15:40:01 +0800

    get rid fo some ES6

--- src/bind-event.js
+++ src/bind-event.js
@@ -10,11 +10,11 @@
 
     "use strict";
 
-    return (elm, evt, listener, options) => {
+    return function (elm, evt, listener, options) {
         if (!elm || !evt || !listener) return;
 
-        const evtUpperCase = evt.substr(0, 1).toUpperCase() + evt.substr(1);
-        const events = [
+        var evtUpperCase = evt.substr(0, 1).toUpperCase() + evt.substr(1);
+        var events = [
             evt,
             'o' + evtUpperCase,
             'moz' + evtUpperCase,
@@ -22,9 +22,9 @@
             'ms' + evtUpperCase
         ];
 
-        let theEvt = false;
+        var theEvt = false;
 
-        events.some(e => {
+        events.some(function (e) {
             if ('on' + e in window) theEvt = e;
             return theEvt;
         });
